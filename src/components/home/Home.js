f9636f73f5e1ee6eabfef423985063c31acf60c0
WildJavaScript <calotadra@gmail.com>
2021-07-17 13:40:14 +0300

    Major bug

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,7 +4,8 @@ import React, { useEffect, useState } from "react";
 import { PaperStyle } from "./Home.style";
 
 function Home() {
-  const [input, setInput] = useState("");
+  const [task, setTask] = useState("");
+  const [todos, setTodos] = useState([]);
   //   const [isDone, setIsDone] = useState(false);
   useEffect(() => {
     axios
@@ -14,15 +15,18 @@ function Home() {
   let sendTodo = () => {
     axios
       .post("https://todo-application-2.herokuapp.com/action", {
-        name: input,
+        name: task,
         isDone: false,
         personId: window.localStorage.getItem("userId"),
       })
       .then((res) => {
         console.log(res);
-        setInput("");
+        setTodos([...todos, res.data.name]);
+        setTask("");
+        console.log(todos);
       });
   };
+
   return (
     <div>
       <Grid>
@@ -35,8 +39,8 @@ function Home() {
             <TextField
               label="Create a new tast"
               fullWidth
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
+              value={task}
+              onChange={(e) => setTask(e.target.value)}
             ></TextField>
             <Button
               type="button"
@@ -49,7 +53,13 @@ function Home() {
             </Button>
           </Grid>
           <Paper>
-            <Grid align="center"></Grid>
+            <Grid align="center">
+              {/* <ul>
+                {todos.map((t) => {
+                  return <li>{t.value}</li>;
+                })}
+              </ul> */}
+            </Grid>
           </Paper>
         </PaperStyle>
       </Grid>
